d71c5db8a5ac3e239ac2457ce35345de4accad25
Tiziano <tizianocitro98@gmail.com>
2020-11-25 17:03:22 +0100

    Data from 2020-11-22

--- twitter.py
+++ twitter.py
@@ -58,7 +58,7 @@ def obtain_tweets(date_since, date_until, with_duplicate_path, without_duplicate
             tweet_user = api.get_user(tweet.user.screen_name)
 
             csv_file_writers_with_duplicate[team].writerow(
-                [tweet.user.screen_name, tweet_user.name, tweet.user.location, str(tweet.created_at)[:10]]) # "2020-11-22"])
+                [tweet.user.screen_name, tweet_user.name, tweet.user.location, str(tweet.created_at)[:10]])
 
     # Save tweets that are not from the same user in order to get the different users which have published
     for team in teams:
